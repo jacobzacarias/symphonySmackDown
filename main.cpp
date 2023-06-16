e33031556f78d9860211fd511e9cdf84f849447f
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2023-06-16 09:11:42 -0600

    Formatting updates to main.cpp
    The handleGestureInput() function is updated to call the corresponding swipes based on the gestureType. The triggerHapticFeedback() function is now before the main() function to prevent the duplicate main() issues. The handleGestureInput() function now happens before the main() function so it is defined first, and then used.
    The functions calculateDamage(), playIdleAnimation(), playWinAnimation(), playLoseAnimation(), parryBlock() and counterAttack() were moved to more appropriate locations.

--- main.cpp
+++ main.cpp
@@ -3,95 +3,45 @@ using namespace std;
 
 // Function to handle swipe up
 void swipeUp() {
-   cout << "Swiped Up!" << endl;
+    cout << "Swiped Up!" << endl;
 }
 
 // Function to handle swipe down
 void swipeDown() {
-   cout << "Swiped Down!" << endl;
+    cout << "Swiped Down!" << endl;
 }
 
 // Function to handle swipe left
 void swipeLeft() {
-   cout << "Swiped Left!" << endl;
+    cout << "Swiped Left!" << endl;
 }
 
 // Function to handle swipe right
 void swipeRight() {
-   cout << "Swiped Right!" << endl;
-}
-
-int main() {
-   // Code to detect user input
-   // Use the following functions to call the corresponding function based on user swipe
-   swipeUp();
-   swipeDown();
-   swipeLeft();
-   swipeRight();
-   return 0;
+    cout << "Swiped Right!" << endl;
 }
 
 // Function to trigger haptic feedback
 void triggerHapticFeedback() {
-  // Code to trigger haptic feedback on the device
+    // Code to trigger haptic feedback on the device
     cout << "Haptic feedback triggered!" << endl;
 }
 
-int main() {
-    // Game loop
-    while (true) {
-        // Game logic
-        bool playerHitOpponent = false;
-        bool playerHitObstacle = true;
-
-        // Check if player hit an opponent
-        if (playerHitOpponent) {
-            // Trigger haptic feedback
-            triggerHapticFeedback();
-            cout << "Player hit an opponent!" << endl;
-        }
-        
-        // Check if player hit an obstacle
-        if (playerHitObstacle) {
-            // Trigger haptic feedback
-            triggerHapticFeedback();
-            cout << "Player hit an obstacle!" << endl;
-        }
-    }
-    
-    return 0;
-}
-        
 // Function to calculate damage
 int calculateDamage(int attackType) {
     int damage = 0;
-    
+
     // Calculate damage based on attack type
     if (attackType == 1) {
-        damage = 10; // Light attack
+        damage = 5; // Light attack
     } else if (attackType == 2) {
-        damage = 20; // Medium attack
+        damage = 10; // Medium attack
     } else if (attackType == 3) {
-        damage = 30; // Heavy attack
+        damage = 15; // Heavy attack
     }
-    
+
     return damage;
 }
-
-int main() {
-// Game loop
-    while (true) {
-        // Game logic
-        int opponentAttackType = 2; // Assume opponent uses medium attack
-        int playerHealth = 100;
-        
-        // Calculate damage based on opponent attack type
-        int damage = calculateDamage(enemyAttackType);
-        cout << "Opponent used attack type " << opponentAttackType << ", causing " << damage << " damage!" << endl;
-        
-        // Subtract damage from player health
-        playerHealth -= damage;
-        cout << "Player health is now " << playerHealth << endl;
 
 // Function to handle frame perfect parry blocking
 void parryBlock() {
@@ -105,65 +55,10 @@ void counterAttack() {
     cout << "Counter attack successful!" << endl;
 }
 
-int main() {
-    // Game loop
-    while (true) {
-        // Game logic
-        bool opponentAttackIncoming = true;
-        int opponentAttackType = 2; // Assume enemy uses medium attack
-        bool playerParryBlock = false;
-        int playerHealth = 100;
-        
-        // Check if enemy attack incoming
-        if (opponentAttackIncoming) {
-            // Check if player performs a frame perfect parry block
-            if (playerParryBlock) {
-                // Perform a counter attack
-                counterAttack();
-                cout << "Player countered enemy attack!" << endl;
-            } else {
-                // Calculate damage based on enemy attack type
-                int damage = 0;
-                if (opponentAttackType == 1) {
-                    damage = 10; // Light attack
-                } else if (opponentAttackType == 2) {
-                    damage = 20; // Medium attack
-                } else if (opponentAttackType == 3) {
-                    damage = 30; // Heavy attack
-                }
-                
-                // Subtract damage from player health
-                playerHealth -= damage;
-                cout << "Opponent used attack type " << opponentAttackType << ", causing " << damage << " damage!" << endl;
-                cout << "Player health is now " << playerHealth << endl;
-            }
-        }
-    }
-   return 0;
-}
-
 // Function to play idle animation
 void playIdleAnimation() {
     // Code to play idle animation
     cout << "Playing idle animation" << endl;
-}
-
-int main() {
-    // Game loop
-    while (true) {
-        // Game logic
-        int timer = 0;
-        
-        // Check if it's time to play the idle animation
-        if (timer % 60 == 0) {
-            // Play idle animation
-            playIdleAnimation();
-        }
-        // Increment timer
-        timer++;
-    }
-    
-    return 0;
 }
 
 // Function to play win animation
@@ -178,73 +73,32 @@ void playLoseAnimation() {
     cout << "Playing lose animation" << endl;
 }
 
-int main() {
-    // Game loop
-    while (true) {
-        // Game logic
-        bool playerWon = true; // Assume player won
-        bool gameOver = true; // Assume game over
-        
-        // Check if game is over and player won
-        if (gameOver && playerWon) {
-            // Play win animation
-            playWinAnimation();
-        } else if (gameOver && !playerWon) {
-            // Play lose animation
-            playLoseAnimation();
-        }
-    }
-    
-    return 0;
-}
-
-    
-    return 0;
-}
-
-// Constants for gesture input types
-const int SWIPE_LEFT = 1;
-const int SWIPE_RIGHT = 2;
-const int SWIPE_UP = 3;
-const int SWIPE_DOWN = 4;
-const int TAP = 5;
-const int SCROLL_UP = 6;
-const int SCROLL_DOWN = 7;
-
 // Function to handle gesture input and output game controls
 void handleGestureInput(int gestureType) {
     // Code to handle gesture input and output game controls
-    switch(gestureType) {
-        case SWIPE_LEFT:
-            // Move left
-            cout << "Move left" << endl;
+    switch (gestureType) {
+        case 1:
+            swipeLeft();
             break;
-        case SWIPE_RIGHT:
-            // Move right
-            cout << "Move right" << endl;
+        case 2:
+            swipeRight();
             break;
-        case SWIPE_UP:
-            // Jump or fly up
-            cout << "Jump or fly up" << endl;
+        case 3:
+            swipeUp();
             break;
-        case SWIPE_DOWN:
-            // Crouch or slide
-            cout << "Crouch or slide" << endl;
+        case 4:
+            swipeDown();
             break;
-        case TAP:
-            // Attack or interact
-            cout << "Attack or interact" << endl;
+        case 5:
+            triggerHapticFeedback();
             break;
-        case SCROLL_UP:
-            // Activate shield or power-up
-            cout << "Activate shield or power-up" << endl;
+        case 6:
+            // Handle scroll up action
             break;
-        case SCROLL_DOWN:
-            // Toggle weapon or item
-            cout << "Toggle weapon or item" << endl;
+        case 7:
+            // Handle scroll down action
             break;
         default:
-            // Invalid gesture type
             cout << "Invalid gesture input" << endl;
             break;
     }
@@ -254,13 +108,13 @@ int main() {
     // Game loop
     while (true) {
         // Game logic
-        
+
         // Check for gesture input
         int gestureType = detectGestureInput();
-        
+
         // Handle gesture input and output game controls
         handleGestureInput(gestureType);
     }
-    
+
     return 0;
 }
