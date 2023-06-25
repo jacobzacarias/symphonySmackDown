56aff0ab48fd17c444dfcd3e9ca34ff9ca70ed23
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2023-06-25 01:44:12 -0600

    Create ambidex.cs
    New file made specifically to provide ambidextrous functions for touch screen taps, swipes, and scroll inputs are based off of the players handedness. 
    Starting with "movePlayer" which of course moves the character currently active in game, the two actions, "performPrimaryAction", "performSecondaryAction" executes the correct action, and "handleInput" determines the appropriate action based off of handedness and user input.

---
+++ ambidex.cs
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+// Function to move the player character
+void movePlayer(const std::string& direction) {
+    std::cout << "Moving player " << direction << std::endl;
+    // Code to move the player in the specified direction
+}
+
+// Function to perform a primary action
+void performPrimaryAction() {
+    std::cout << "Performing primary action" << std::endl;
+    // Code to execute the primary action
+}
+
+// Function to perform a secondary action
+void performSecondaryAction() {
+    std::cout << "Performing secondary action" << std::endl;
+    // Code to execute the secondary action
+}
+
+// Ambidextrous function to handle input based on the user's handedness
+void handleInput(bool isLeftHanded, const std::string& input) {
+    if (isLeftHanded) {
+        if (input == "swipeLeft") {
+            movePlayer("left");
+        } else if (input == "swipeRight") {
+            movePlayer("right");
+        } else if (input == "tap") {
+            performPrimaryAction();
+        } else if (input == "doubleTap") {
+            performSecondaryAction();
+        } else {
+            std::cout << "Invalid input" << std::endl;
+        }
+    } else {
+        if (input == "swipeUp") {
+            movePlayer("up");
+        } else if (input == "swipeDown") {
+            movePlayer("down");
+        } else if (input == "tap") {
+            performPrimaryAction();
+        } else if (input == "doubleTap") {
+            performSecondaryAction();
+        } else {
+            std::cout << "Invalid input" << std::endl;
+        }
+    }
+}
+
+int main() {
+    bool isLeftHanded = false; // Set this based on user preference or device configuration
+
+    // Game loop
+    while (true) {
+        // Receive input from the user (e.g., swipe, tap, double tap)
+        std::string input;
+        std::cout << "Enter input: ";
+        std::cin >> input;
+
+        // Handle input based on user's handedness
+        handleInput(isLeftHanded, input);
+    }
+
+    return 0;
+}
