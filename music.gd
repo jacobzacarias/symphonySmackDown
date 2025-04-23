8b42005ffa8cdd936a8ae9b45f1445495d9a33f8
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2025-04-23 08:50:42 -0500

    Create music.gd
    Initial commit for music.gd

---
+++ music.gd
@@ -0,0 +1,44 @@
+extends CanvasLayer
+
+var songs_dir = 'user://'
+var song_item = load("res://SongPicker/SongItem/SongItem.tscn")
+var discovered_songs = []
+
+func _ready():
+	var listed_dir = global.list_dir(songs_dir)
+	for dir in listed_dir:
+		var info_path = songs_dir + dir + '/info.dat'
+		var file = File.new()
+		file.open(info_path, File.READ)
+		var result = ''
+		while not file.eof_reached():
+			var line = file.get_line()
+			result += line
+		file.close()
+		var data = parse_json(result)
+		print(data['_songAuthorName'])
+		var item_name = data['_songName'] + " by " + data['_songAuthorName']
+		var sprite = crate_sprite(songs_dir + dir + '/' + data['_coverImageFilename'])
+		
+		discovered_songs.append([data, songs_dir + dir, sprite])
+		
+		var item = song_item.instance()
+		item.init(data, sprite)
+		$SongList.add_child(item)
+		$Cover.texture = sprite
+		
+		print(songs_dir + dir + '/' + data['_coverImageFilename'])
+
+#func _on_ItemList_item_selected(index):
+#	$Cover.texture = discovered_songs[index][2]
+
+func crate_sprite(path):
+	var img = Image.new()
+	var err = img.load(path)
+	if(err != 0):
+		print("error loading the image: " + path)
+		return null
+	var img_tex = ImageTexture.new()
+	img_tex.create_from_image(img)
+
+	return img_tex
